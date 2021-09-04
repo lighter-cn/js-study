d50a08a3fc28b05f60203db74b173578186e946c
lighter <54133131+lighter-cn@users.noreply.github.com>
2021-09-04 23:59:42 +0900

    0904

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("hello");
+// strict mode
+"use strict";
+// mistypedVariable = 42; // ReferenceError
+
+// 大文字と小文字を区別する
+const ex = "hoge";
+const EX = "fuga";
+console.log(ex); // output: hoge
+
+// 一行コメント
+
+/* 
+  複数行コメント
+*/
+
+/*------------------------------
+  変数と宣言
+------------------------------*/
+// const
+const name = "Musashi";
+
+// ,で区切って複数同時に定義できる
+const name1 = "John",
+  name2 = "Jack";
+
+// 再代入でエラーになる
+// const errorTest = "abc";
+// errorTest = "def"; // invalid assignment to const 'errorTest'
+
+// let
+
+// 初期値なしの場合
+let letUndefined;
+console.log(letUndefined);
+
+// 後から値を代入できる
+let letAfterDefined;
+letAfterDefined = 10;
+console.log(letAfterDefined);
