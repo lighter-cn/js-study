ff9d6abaed731a1034fbddad63e9b8291d0c650d
lighter <54133131+lighter-cn@users.noreply.github.com>
2021-09-05 21:53:24 +0900

    0905

--- main.js
+++ main.js
@@ -37,3 +37,71 @@ console.log(letUndefined);
 let letAfterDefined;
 letAfterDefined = 10;
 console.log(letAfterDefined);
+
+// constは再代入できる
+const re_substitution = { key: "hoge" };
+re_substitution.key = "fuga";
+console.log(re_substitution); // output: "fuga"
+
+/*------------------------------
+  データ型とリテラル
+------------------------------*/
+
+console.log(typeof true); // boolean
+console.log(typeof 42); // number
+console.log(typeof "string"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof ["配列"]); // object
+console.log(typeof { key: "value" }); // object
+console.log(typeof function () {}); // function
+
+// リテラル表現を持つ4つのプリミティブ型
+console.log(true);
+console.log(false);
+
+console.log(10);
+console.log(0b110);
+console.log(0o77);
+console.log(0x2fc);
+
+console.log(0.123);
+console.log(1.23e2);
+
+console.log(1_234_567_890);
+
+console.log("文字列");
+console.log("string");
+console.log("改行を\nここでする");
+console.log(`ここで
+改行
+してます`);
+const test_back_quote = "test";
+console.log(`変数の値は${test_back_quote}です`);
+
+let nullTest = null;
+console.log(nullTest);
+
+// 中身が空のオブジェクトを作成
+const obj = {};
+
+// ドット記法とブラケット記法の値参照
+const obj2 = { key: "value" };
+console.log(obj2.key);
+console.log(obj2["key"]);
+
+// 数字の文字列なのでブラケット記法でのみ参照できる
+const obj3 = { "12": "hoge" };
+console.log(obj3["12"]);
+
+// 配列リテラル
+const array = [1, 2, 3];
+console.log(array[2]); // output : 3
+
+// 正規表現リテラル
+const numberRegExp = /\d+/; // 1文字以上の数字にマッチする正規表現
+console.log(numberRegExp.test("123")); // output: true
+
+// リテラルを使ったプリミティブ型でもプロパティを参照できる
+const testPrimitive = "string length sample";
+console.log(testPrimitive.length);
