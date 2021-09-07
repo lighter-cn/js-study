3a05a2c1fe7945ce05e60492c1b97a1a30f6bd1e
lighter <54133131+lighter-cn@users.noreply.github.com>
2021-09-07 23:39:19 +0900

    0907

--- main2.js
+++ main2.js
@@ -1,4 +1,3 @@
-console.log("main2.js");
 /*------------------------------
   暗黙的な型変換
 ------------------------------*/
@@ -6,3 +5,156 @@ console.log("main2.js");
 console.log(1 == true); // true
 console.log(0 == false); // true
 console.log(10 == ["10"]); // true
+
+// 明示的な型変換
+console.log(Boolean("string"));
+console.log(Boolean(0));
+console.log(Boolean(null));
+
+console.log(String(true));
+console.log(String(false));
+console.log(String(undefined));
+console.log(String(null));
+console.log(String(Symbol("something")));
+console.log(String([1, 2, 3]));
+console.log(String({ key: "value" }));
+console.log(String(function () {}));
+
+console.log(Number("123"));
+console.log(Number.parseInt("42px", 10));
+console.log(Number.parseInt("10.5", 10));
+console.log(Number.parseFloat("42.5px"));
+console.log(Number.parseFloat("10.5"));
+
+console.log(Number.parseFloat("abc"));
+console.log(NaN + 10);
+console.log(Number.isNaN(1));
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(false));
+console.log(Number.isNaN({}));
+console.log(Number.isNaN("str"));
+
+console.log(typeof NaN);
+
+/*------------------------------
+  関数と宣言
+------------------------------*/
+function double(value) {
+  return value * 2;
+}
+
+console.log(double(4));
+
+// returnの後に書いた処理は行わない例
+function returnTest() {
+  return;
+  console.log("ここは処理されない");
+}
+const rt = returnTest();
+console.log(rt);
+
+// 仮引数よりも実引数が少ない時の例
+function echo(x) {
+  return x;
+}
+console.log(echo());
+
+// デフォルト引数
+function echoDef(x = "デフォルト値") {
+  return x;
+}
+console.log(echo("hoge"));
+console.log(echoDef());
+
+// 実引数が仮引数よりも多い時の例
+function add(x, y) {
+  return x + y;
+}
+console.log(add(2, 3, 4));
+
+// Rest parametersで引数が配列として代入されている例
+function fn(...args) {
+  console.log(args);
+}
+fn(1, 2, 3, 4, 5);
+
+function fn2(arg, ...args) {
+  console.log(arg);
+  console.log(args);
+}
+fn2(1, 2, 3, 4, 5);
+
+// Spread構文
+function fn3(arg1, arg2, arg3) {
+  console.log(arg1);
+  console.log(arg2);
+  console.log(arg3);
+}
+const arr = ["a", "b", "c"];
+fn3(...arr);
+
+// argumentsの例
+function fn4() {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+}
+fn4(3, 2, 1);
+
+// 引数に分割代入を使った例
+function fn5({ id }) {
+  console.log(id);
+}
+const obj = {
+  id: 42
+};
+fn5(obj);
+
+function fn6([first, second]) {
+  console.log(first);
+  console.log(second);
+}
+const arr2 = ["aa", "bb"];
+fn6(arr2);
+
+// 関数を変数に代入して呼び出す例
+function fn7() {
+  console.log("call fn7");
+}
+const myFunc = fn7;
+myFunc();
+
+// 関数式
+const fn8 = function () {
+  console.log("call fn8");
+};
+fn8();
+
+// 再帰的な処理を行う関数式
+const fn9 = function inner(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return inner(n - 1) * n;
+};
+console.log(fn9(4));
+
+// arrow function
+const af1 = (x, y) => {
+  return x * y;
+};
+console.log(af1(2, 4));
+
+const af2 = (x) => x * 3;
+console.log(af2(7));
+
+let arr_ = [1, 2, 3];
+const doubleArr = arr_.map((x) => x * 2);
+console.log(doubleArr);
+
+// コールバック関数
+const arr3 = [1, 2, 3];
+const output = (value) => {
+  console.log(value);
+};
+arr3.forEach(output);
